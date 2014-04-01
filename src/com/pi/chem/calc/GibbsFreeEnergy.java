2f7f5b9a1113e47b0c43605d0cbcc0beeda320f6
Westin Miller <equinoxscripts@gmail.com>
2014-04-01 12:33:54 -0700

    Added features for Gibbs Free Energy.

--- src/com/pi/chem/calc/GibbsFreeEnergy.java
+++ src/com/pi/chem/calc/GibbsFreeEnergy.java
@@ -13,57 +13,71 @@ public class GibbsFreeEnergy {
 			.compile("^([0-9]*)(.*)");
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				System.in));
-		String eq = reader.readLine();
-		String[] parts = eq.split("(->|→)");
-		if (parts.length != 2) {
-			throw new IllegalArgumentException("Must be an equation...");
-		}
-		float eqFormation = 0;
-		float eqEntropy = 0;
-		for (int i = 0; i < parts.length; i++) {
-			String[] chunks = parts[i].trim().split("( |->|\\+|→)");
-			float totalFormation = 0;
-			float totalEntropy = 0;
-			for (String s : chunks) {
-				if (s.trim().length() > 0) {
-					Matcher m = MOLECULE_COEFF.matcher(s.trim());
-					if (m.find()) {
-						String coeffS = m.group(1).trim();
-						String mole = m.group(2);
-						float coeff = 1;
-						if (coeffS.length() > 0) {
-							coeff = Float.valueOf(coeffS);
+		while (true) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					System.in));
+			String eq = reader.readLine();
+			String[] parts = eq.split("(->|→)");
+			if (parts.length != 2) {
+				throw new IllegalArgumentException("Must be an equation...");
+			}
+			float eqFormation = 0;
+			float eqEntropy = 0;
+			for (int i = 0; i < parts.length; i++) {
+				String[] chunks = parts[i].trim().split("( |->|\\+|→)");
+				float totalFormation = 0;
+				float totalEntropy = 0;
+				for (String s : chunks) {
+					if (s.trim().length() > 0) {
+						Matcher m = MOLECULE_COEFF.matcher(s.trim());
+						if (m.find()) {
+							String coeffS = m.group(1).trim();
+							String mole = m.group(2);
+							float coeff = 1;
+							if (coeffS.length() > 0) {
+								coeff = Float.valueOf(coeffS);
+							}
+							float heat = ThermodynamicQuantities
+									.getHeatOfFormation(mole);
+							float entropy = ThermodynamicQuantities
+									.getStandardEntropy(mole);
+							totalFormation += coeff * heat;
+							totalEntropy += coeff * entropy;
 						}
-						float heat = ThermodynamicQuantities
-								.getHeatOfFormation(mole);
-						float entropy = ThermodynamicQuantities
-								.getStandardEntropy(mole);
-						totalFormation += coeff * heat;
-						totalEntropy += coeff * entropy;
 					}
 				}
+				eqEntropy -= totalEntropy * (i == 0 ? 1 : -1);
+				eqFormation -= totalFormation * (i == 0 ? 1 : -1);
 			}
-			eqEntropy -= totalEntropy * (i == 0 ? 1 : -1);
-			eqFormation -= totalFormation * (i == 0 ? 1 : -1);
-		}
-		System.out.println("Standard Entropic Change (ΔS°):\t" + eqEntropy
-				* 1000 + "\tJ / (mol ⋅ K)");
-		System.out.println("Heat of Formation (ΔHf°):\t" + eqFormation
-				+ "\tkJ / mol");
-		System.out.println("Gibbs Standard Free Energy (ΔG°):\t"
-				+ (eqFormation - eqEntropy * 298.15) + "\tkJ/mol");
-		while (true) {
-			try {
-				System.out.print("Gibbs Free Energy @ Temperature (K)?\t");
-				float temp = Float.valueOf(reader.readLine());
-				System.out.println("Gibbs Free Energy (ΔG):\t"
-						+ (eqFormation - eqEntropy * temp) + "\tkJ/mol");
-			} catch (Exception e) {
-				break;
+			System.out.println("Standard Entropic Change (ΔS°):\t" + eqEntropy
+					* 1000 + "\tJ / (mol ⋅ K)");
+			System.out.println("Heat of Formation (ΔHf°):\t" + eqFormation
+					+ "\tkJ / mol");
+			System.out.println("Gibbs Standard Free Energy (ΔG°):\t"
+					+ (eqFormation - eqEntropy * 298.15) + "\tkJ/mol");
+			System.out.println("Standard Total Entropy (ΔHt°):\t"
+					+ (eqEntropy + (-eqFormation / 298.15)) * 1000.0
+					+ "\tJ / (mol ⋅ K)");
+			System.out.println("Standard Reaction Constant (K°):\t"
+					+ Math.exp((eqFormation - eqEntropy * 298.15) * 1000.0
+							/ (8.314 * 298.15)));
+			System.out.println();
+			while (true) {
+				try {
+					System.out.print("Temperature (K)?\t");
+					float temp = Float.valueOf(reader.readLine());
+					System.out.println("Gibbs Free Energy (ΔG):\t"
+							+ (eqFormation - eqEntropy * temp) + "\tkJ/mol");
+					System.out.println("Total Entropy (ΔHt):\t"
+							+ (eqEntropy + (-eqFormation / temp)) * 1000.0
+							+ "\tJ / (mol ⋅ K)");
+					System.out.println("Reaction Constant (K):\t"
+							+ Math.exp((eqFormation - eqEntropy * temp)
+									* 1000.0 / (-8.314 * temp)));
+				} catch (Exception e) {
+					break;
+				}
 			}
 		}
-		reader.close();
 	}
 }
